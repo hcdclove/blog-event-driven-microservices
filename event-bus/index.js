b2fab7f1a35167663735cd0bf258b176e2af6c39
hcdclove <131356756+hcdclove@users.noreply.github.com>
2023-07-13 07:58:21 +0200

    Blog V4.0.1

--- event-bus/index.js
+++ event-bus/index.js
@@ -17,18 +17,11 @@ app.post('/events', (req, res) => {
 
 	events.push(event);
 
-	axios.post('http://localhost:4000/events', event).catch((err) => {
-		console.log(err.message);
-	});
-	axios.post('http://localhost:4001/events', event).catch((err) => {
-		console.log(err.message);
-	});
-	axios.post('http://localhost:4002/events', event).catch((err) => {
-		console.log(err.message);
-	});
-	axios.post('http://localhost:4003/events', event).catch((err) => {
-		console.log(err.message);
-	});
+	axios.post('http://posts-clusterip-srv:4000/events', event);
+	// 	axios.post('http://localhost:4001/events', event);
+	// 	axios.post('http://localhost:4002/events', event);
+	// 	axios.post('http://localhost:4003/events', event);
+
 	res.send({ status: 'OK' });
 });
 
